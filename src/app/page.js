1247bd83152613b6d03be34f718fe248437108aa
Randal <randal.ramirez4705@alumnos.udg.mx>
2024-06-17 15:51:40 -0600

    a

--- src/app/page.js
+++ src/app/page.js
@@ -17,7 +17,6 @@ export default function Home() {
             // target="_blank"
             rel="noopener noreferrer"
           >
-            <div></div>
             <Image
               src="/mondasi-logo.png"
               alt="Vercel Logo"
